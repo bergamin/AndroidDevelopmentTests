d138351f14615949cb8cee988ac204cfa18ab2a8
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2017-11-27 09:41:04 -0200

    - Classes Added: Contact, FormHelper (for making the data management in the Form activity easier) - Started building Form's functions - Reorganized ids in the activity's xml - Created a menu - Added some more translations

--- Kotlin/ContactList/app/src/main/java/com/bergamin/contactlist/FormActivity.kt
+++ Kotlin/ContactList/app/src/main/java/com/bergamin/contactlist/FormActivity.kt
@@ -1,12 +1,50 @@
 package com.bergamin.contactlist
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.Menu
+import android.view.View
+import kotlinx.android.synthetic.main.activity_form.*
+import java.io.File
 
-class FormActivity : AppCompatActivity() {
+class FormActivity : AppCompatActivity(), View.OnClickListener {
+
+    var helper: FormHelper? = null
+    var photoPath: String? = null
+
+    companion object {
+        val CAMERA_REQUEST = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form)
+        helper?.activity = this
+
+        var contact: Contact = intent.getSerializableExtra("contact") as Contact
+
+        helper?.fillForm(contact)
+
+    }
+    override fun onClick(view: View?) {
+        photoPath = getExternalFilesDir(null).path + "/" + System.currentTimeMillis() + ".jpg"
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        var photoFile = File(photoPath)
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
+        startActivityForResult(intent,CAMERA_REQUEST)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK){
+            helper?.loadImage(photoPath!!)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.for)
     }
 }
---
+++ Kotlin/ContactList/app/src/main/java/com/bergamin/contactlist/FormHelper.kt
@@ -0,0 +1,52 @@
+package com.bergamin.contactlist
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.widget.ImageView
+import android.widget.TextView
+
+/**
+ * Created by gbergamin on 24/11/2017.
+ */
+class FormHelper(
+        var activity: FormActivity,
+        var contact: Contact) {
+
+    val nameTxt: TextView
+    val addressTxt: TextView
+    val phoneTxt: TextView
+    val websiteTxt: TextView
+    val photoImg: ImageView
+
+    init {
+        nameTxt = activity.findViewById(R.id.nameTxt)
+        addressTxt = activity.findViewById(R.id.addressTxt)
+        phoneTxt = activity.findViewById(R.id.phoneTxt)
+        websiteTxt = activity.findViewById(R.id.websiteTxt)
+        photoImg = activity.findViewById(R.id.photoImg)
+
+        contact.name = nameTxt.text.toString()
+        contact.address = addressTxt.text.toString()
+        contact.phone = phoneTxt.text.toString()
+        contact.webSite = websiteTxt.text.toString()
+        contact.photoPath = photoImg.tag as String
+    }
+    fun fillForm(contact: Contact){
+        nameTxt.text = contact.name
+        addressTxt.text = contact.address
+        phoneTxt.text = contact.phone
+        websiteTxt.text = contact.webSite
+
+        loadImage(contact.photoPath)
+        this.contact = contact
+    }
+
+    fun loadImage(path: String) {
+        var bitmap = BitmapFactory.decodeFile(path)
+        bitmap = Bitmap.createScaledBitmap(bitmap,300,300, true)
+
+        photoImg.setImageBitmap(bitmap)
+        photoImg.scaleType = ImageView.ScaleType.FIT_XY
+        photoImg.tag = path
+    }
+}
