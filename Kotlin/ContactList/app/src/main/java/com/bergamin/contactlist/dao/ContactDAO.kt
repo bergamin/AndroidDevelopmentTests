66b87ee83471b650a3cb10cf1606e9c82bd05638
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2018-01-11 16:33:52 -0200

    Finances project creation

--- Kotlin/ContactList/app/src/main/java/com/bergamin/contactlist/dao/ContactDAO.kt
+++ Kotlin/ContactList/app/src/main/java/com/bergamin/contactlist/dao/ContactDAO.kt
@@ -4,7 +4,6 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import android.util.Log
 import com.bergamin.contactlist.R
 import com.bergamin.contactlist.model.Contact
 import com.bergamin.contactlist.util.FileHelper
