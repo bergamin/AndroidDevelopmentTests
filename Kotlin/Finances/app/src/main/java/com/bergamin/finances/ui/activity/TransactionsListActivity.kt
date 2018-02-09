5dd05e13dda8aaa9cc1f3cf28ccafabea544f14a
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2018-02-09 17:30:56 -0200

    Commiting all new files for working during Carnival holiday

---
+++ Kotlin/Finances/app/src/main/java/com/bergamin/finances/ui/activity/TransactionsListActivity.kt
@@ -0,0 +1,15 @@
+package com.bergamin.finances.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bergamin.finances.R
+
+/**
+ * Created by Guilherme Taffarel Bergamin on 12/01/2018.
+ */
+class TransactionsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transactions_list)
+    }
+}
