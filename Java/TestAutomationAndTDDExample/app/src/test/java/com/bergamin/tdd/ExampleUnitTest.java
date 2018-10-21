8bdf0c842352c9a3786e79323a904db1c94ea981
Guilherme Taffarel Bergamin <taffarel@outlook.com>
2018-10-21 19:58:17 -0300

    Added some other assertion examples to the ExampleUnitTest test class

--- Java/TestAutomationAndTDDExample/app/src/test/java/com/bergamin/tdd/ExampleUnitTest.java
+++ Java/TestAutomationAndTDDExample/app/src/test/java/com/bergamin/tdd/ExampleUnitTest.java
@@ -2,7 +2,12 @@ package com.bergamin.tdd;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +16,12 @@ import static org.junit.Assert.assertEquals;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
+    public void assertionExamples() {
+        String nullVar = null;
+
         assertEquals(4, 2 + 2);
+        assertTrue(4 == 2 + 2);
+        assertThat(2 + 2, equalTo(4));
+        assertNull(nullVar);
     }
 }
