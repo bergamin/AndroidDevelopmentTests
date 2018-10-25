6fb425711c30a59de2f346dbfa89f2da28e63758
DBSERVER\gbergamin <gbergamin@dbserver.com.br>
2018-10-25 17:04:43 -0200

    Refactoring

--- Java/TestAutomationAndTDDExample/app/src/main/java/com/bergamin/tdd/model/Bid.java
+++ Java/TestAutomationAndTDDExample/app/src/main/java/com/bergamin/tdd/model/Bid.java
@@ -21,15 +21,31 @@ public class Bid implements Serializable, Comparable {
     @Override
     public int compareTo(@NonNull Object o) {
         Bid bid = (Bid) o;
-        if (value > bid.getValue()) {
-            return -1;
-        } else if (value < bid.getValue()) {
-            return 1;
-        }
-        return 0;
+        return Double.compare(bid.getValue(), value);
     }
 
     public User getUser() {
         return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Bid bid = (Bid) o;
+
+        if (Double.compare(bid.value, value) != 0) return false;
+        return user.equals(bid.user);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = user.hashCode();
+        temp = Double.doubleToLongBits(value);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
