2ba7f79d2cc5c4dbc3e6865b2f6fcf49afa2c0d1
Guilherme Taffarel Bergamin <guilherme.bergamin@pucrs.br>
2020-03-09 18:53:30 -0300

    Added database initialisation to the main call

--- Flutter/finances/lib/main.dart
+++ Flutter/finances/lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:finances/services/DB.dart';
 import 'package:finances/ui/HomeWidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DB.init();
+  runApp(FinancesApp());
+}
 
-class MyApp extends StatelessWidget {
+class FinancesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
